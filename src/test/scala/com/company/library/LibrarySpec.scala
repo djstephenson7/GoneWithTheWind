c3b412467f07f8e86f490b94515a03ff3fd7c7da
djstephenson7 <dan_0502@hotmail.co.uk>
2019-04-10 10:31:56 +0100

    Test files

--- src/test/scala/com/company/library/LibrarySpec.scala
+++ src/test/scala/com/company/library/LibrarySpec.scala
@@ -3,10 +3,18 @@ package com.company.library
 import org.scalatest.FunSuite
 import org.scalatest.Matchers._
 
-class LibrarySpec extends FunSuite {
 
-  test("some words to describe my test") {
-    "aString" shouldBe "aString"
+class LibrarySpec extends FunSuite {
+  test("Display Books") {
+    val library = new Library
+    library.list_books shouldBe a [List[_]]
   }
 
+  test("Find book by full name") {
+    val mockBookList: List[Book] = List(Book("mockName1", "mockAuthor1", "ISBN1"), Book("mockName2", "mockAuthor2", "ISBN2"))
+    val library = new Library
+    library.search_title("mockName1", mockBookList) shouldBe Some(Book("mockName1", "mockAuthor1", "ISBN1"))
+    library.search_title("mockName2", mockBookList) shouldBe Some(Book("mockName2", "mockAuthor2", "ISBN2"))
+    library.search_title("mockName3", mockBookList) shouldBe None
+  }
 }
