a929aeb12464feb8e17e42f3cca40d1ec8c45476
djstephenson7 <dan_0502@hotmail.co.uk>
2019-04-10 10:31:12 +0100

    Searh by title

--- src/main/scala/com/company/library/Library.scala
+++ src/main/scala/com/company/library/Library.scala
@@ -2,5 +2,9 @@ package com.company.library
 
 class Library {
   def list_books = Books.all
+
+  def search_title(name: String, books: List[Book]): Option[Book] = {
+    books.find(a => a.title == name)
+  }
 }
 
